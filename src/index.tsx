1293f6739cafedc9bcbaeed24375f55bd4123bb3
CSigitas <90483186+CSigitas@users.noreply.github.com>
2022-03-20 00:43:56 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import { fetchAllCountries } from "./countriesService";
 import "./index.css";
+import React from "react";
 import App from "./App";
 
 ReactDOM.render(
